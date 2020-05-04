630550a5e88abf83ae92edf8e8e70a4ae9326186
Merve <mervekarabulut@lonca.works>
2020-05-04 15:53:59 +0300

    create index.html, rewrite restarurant and food classes

--- food delivery example/index.js
+++ food delivery example/index.js
@@ -1,24 +1,29 @@
-restaurant = class{
-    constructor(name){
-    this.name= name
-    this.menu=[]
-    }
+// classes = restaurant, food, customer, food 
+//interactions 
+// 1) a restaurant has a lot of food that the customer can order
+// 2) a customer can order food
+// 3) the driver picks up the foods from the restaurant and delivers it to the customer
 
-    add(food){
-        this.menu.push(food)
-        //food.list.push(this) what is the function of this code? 
-
-    }
-}
-
-food = class{
-    constructor(name,cost){
-        this.name=name
-        this.cost=cost
-        this.list=[]
+Restaurant = class{
+    constructor(name, menu){
+    this.name = name
+    this.menu = []  // menu is an array that contains foods
     }
 }
 
-pizza = new food ('pizza','25 TL')
-hamburger = new food('hamburger','25 TL')
-abc = new restaurant('abc')
+abc = new Restaurant('ABC')
+
+Food = class{
+    constructor(name, cost){
+        this.name =name
+        this.cost =cost
+        this.list = []  //list of food
+    }
+    add(restaurant){
+        this.restaurant = restaurant
+        restaurant.menu.push(this)  // I can add foods to restaurant by add functıon
+    }
+}
+
+pizza = new Food ('Pizza', '25 TL')
+hamburger = new Food ('Hamburger', '35 TL')
