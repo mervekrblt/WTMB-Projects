eb71c792afbfce7cf984b02fd2b27d87ffdb1bc4
Merve <mervekarabulut@lonca.works>
2020-05-07 13:49:30 +0300

    add driver class

--- food delivery example/index.js
+++ food delivery example/index.js
@@ -5,10 +5,11 @@
 // 3) the driver picks up the foods from the restaurant and delivers it to the customer
 
 Restaurant = class{
-    constructor(name, menu){
+    constructor(name){
     this.name = name
     this.menu = []  // menu is an array that contains foods
     this.orderlist =[] // access to customer class with ** code
+    this.drivers = []  // list of drivers
     }
 }
 
@@ -33,13 +34,32 @@ Customer = class{
     constructor(name, adress){
         this.name = name
         this.adress = adress
-        this.orderlist= []  // list of customers orderlist
+        this.orderlist= []  // list of customer's orderlist
     }
     order(food, restaurant){
         this.food = food
         restaurant.orderlist.push(this) // ** code
-        this.orderlist.push(food) // add foods to customer.orderlist
+        this.orderlist.push(food.name) // add food.name to customer.orderlist by this code
     }
 }
 
 merve = new Customer('Merve', 'Ankara')
+
+Driver = class {
+    constructor(name){
+        this.name = name
+    }
+    accessInformation(customer){  //driver can get access customer information by this function
+        console.log(customer.name, customer.orderlist, customer.adress)
+    }
+    getHiredBy(restaurant){  // driver can be hired by restaurant 
+        this.restaurant=restaurant.name  // add restaurant name into driver information
+        restaurant.drivers.push(this) // I can add to driver's information to restaurant.drivers 
+    }
+}
+
+ali = new Driver ('Ali')
+ali.getHiredBy(abc)
+pizza.add(abc)
+merve.order(pizza,abc)
+merve.order(hamburger,abc)
