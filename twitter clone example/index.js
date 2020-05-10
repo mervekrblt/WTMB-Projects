2b61fcb06bff33b879935213409a7c26705220f2
Merve <mervekarabulut@lonca.works>
2020-05-10 16:44:05 +0300

    first commit

---
+++ twitter clone example/index.js
@@ -0,0 +1,7 @@
+// classes = Tweet, User, Like
+// interactions 
+// a User has many Tweets and can create new Tweets
+// a User can like a Tweet by creating a Like
+// each Like has one User and one Tweet
+// a Tweet can have many Likes
+// a User can have many Likes
