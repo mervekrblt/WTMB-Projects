87a1fcfe117913cb1372b6f31f1dfb1fdc780167
Merve <mervekarabulut@lonca.works>
2020-05-13 21:20:03 +0300

    Add User, Tweet and Like classes

--- twitter clone example/index.js
+++ twitter clone example/index.js
@@ -5,3 +5,41 @@
 // each Like has one User and one Tweet
 // a Tweet can have many Likes
 // a User can have many Likes
+
+User = class{
+    constructor(name){
+        this.name = name
+        this.tweets =[]  // list of tweets
+        this.likes =[]
+    }
+}
+
+merve = new User('Merve')
+melis = new User('Melis')
+olcay = new User('Olcay')
+
+Tweet = class {
+    constructor(name){
+        this.name = name
+    }
+    createdBy(User){            // a user can create new tweets
+        User.tweets.push(this) // with this code, I can collect tweets in an array
+    }
+}
+
+tweet1 = new Tweet('Welcome')
+tweet2 = new Tweet('Hello')
+tweet3 = new Tweet('Hey')
+
+tweet1.createdBy(merve)
+tweet2.createdBy(merve)
+
+Like = class{
+    constructor(user, tweet){
+    this.user= user
+    this.tweet = tweet
+    }
+    }
+
+like1 = new Like ('Melis', 'tweet1')
+like2 = new Like ('Olcay', 'tweet2')
