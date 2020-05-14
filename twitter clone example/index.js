b38e5d93e4d243fa2e6f86d57a48dfc94d1bea55
Merve <mervekarabulut@lonca.works>
2020-05-14 16:24:36 +0300

    Add arrays in Tweet class && Create for function in Like class && Add comment lines

--- twitter clone example/index.js
+++ twitter clone example/index.js
@@ -9,8 +9,8 @@
 User = class{
     constructor(name){
         this.name = name
-        this.tweets =[]  // list of tweets
-        this.likes =[]
+        this.tweets =[]  // list of user's tweets
+        this.likes =[]   // list of user's likes
     }
 }
 
@@ -21,25 +21,36 @@ olcay = new User('Olcay')
 Tweet = class {
     constructor(name){
         this.name = name
+        this.likes = [] // list of tweet's likes
+        this.user = []  // list of user of tweet
     }
-    createdBy(User){            // a user can create new tweets
+    createdBy(User){             // a user can create new tweets
+        this.user.push(User.name)  // add user name into tweet user's array
         User.tweets.push(this) // with this code, I can collect tweets in an array
     }
 }
 
 tweet1 = new Tweet('Welcome')
 tweet2 = new Tweet('Hello')
-tweet3 = new Tweet('Hey')
 
 tweet1.createdBy(merve)
 tweet2.createdBy(merve)
 
 Like = class{
     constructor(user, tweet){
-    this.user= user
+    this.user= user  
     this.tweet = tweet
     }
+    for(user, tweet){  // add a like with "for" function
+        user.likes.push(this) // this like goes to array of user likes
+        tweet.likes.push(this) // this like goes to array of tweet likes
     }
+}
 
 like1 = new Like ('Melis', 'tweet1')
 like2 = new Like ('Olcay', 'tweet2')
+like3 = new Like ('Olcay', 'tweet1')
+
+like1.for(merve, tweet1)
+like2.for(merve, tweet2)
+like3.for(merve, tweet1)
