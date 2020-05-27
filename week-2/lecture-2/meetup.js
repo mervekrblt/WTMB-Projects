cc81d8549dffa8e28015beaf87f268618edd6869
Merve Karabulut <63372370+mervekrblt@users.noreply.github.com>
2020-05-27 22:53:50 +0300

    Delete meetup.js

--- week-2/lecture-2/meetup.js
+++
@@ -1,9 +0,0 @@
-module.exports = class Meetup{
-    constructor(name) {
-      this.name = name
-      this.attendees = []
-    }
-    printAttendeeNames() {
-      this.attendees.forEach(printName)
-    }
-  }
