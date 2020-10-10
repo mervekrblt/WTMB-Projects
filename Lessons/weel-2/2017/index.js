8192b6da8b124a74374c88856ea808a5a7645af8
Merve Karabulut <mervekarabulut@lonca.works>
2020-10-10 17:58:48 +0300

    Add WTMB-2017 week-2 & Install chalk to colorize console

---
+++ Lessons/weel-2/2017/index.js
@@ -0,0 +1,10 @@
+const chalk = require('chalk')
+
+console.log('hello world')
+
+const add = (num1, num2) => num1 + num2
+
+const result = add(4,5)
+
+console.log(result)
+console.log(chalk.bold.magenta('<3 the end <3'))
