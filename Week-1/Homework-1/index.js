c1ff1dd9d42a7ac0b19ed47e93484803d9e057ce
Merve Karabulut <63372370+mervekrblt@users.noreply.github.com>
2020-12-11 15:54:45 +0300

    Use Like object in like method. Create follow method inside User class
    I used Tweet object in tweet method. In this way, I can take the text
    which is a parameter of tweet method, then convert to tweet. But, when I
    declare a variable like const tweet1 = merve.tweet('hello world'), I got
    undefined, because I should use RETURN TWEET to go back tweet
    
    I created Like class and also I used this object into like method like
    what I did for tweet method. I created a like and push this like to
    tweet.
    
    Following and followers parts were also missing. So, I created them

--- Week-1/Homework-1/index.js
+++ Week-1/Homework-1/index.js
@@ -1,68 +1,48 @@
-/*
-Twitter clone
-
-Classes
-    Tweet
-    User
-    Like
-
-Interactions
-    a User has many Tweets and can create new Tweets
-    a User can like a Tweet by creating a Like
-    each Like has one User and one Tweet
-    a Tweet can have many Likes
-    a User can have many Likes
-*/
-
 class User{
     constructor(name, username){
-        this.name =name
-        this.username =username
-        this.followers =[]
-        this.following = []
+        this.name = name
+        this.username = username
         this.tweets = []
-        this.likedTweets = []
+        this.likedTweets =[]
+        this.following = []
+        this.followers =[]
+    }
+    tweet(text){
+        const tweet = new Tweet(this,text)
+        this.tweets.push(tweet)
+        return tweet
+    }
+    like(tweet){
+        const like = new Like(this, tweet)
+        this.likedTweets.push(tweet)
+        tweet.likes.push(like)
+        return like
     }
     follow(user){
         user.followers.push(this)
         this.following.push(user)
     }
-    tweet(text){
-        const tweet = new Tweet(this.username,text)
-        this.tweets.push(tweet)
-    }
-    like(user, tweet){
-        const likedTweet = new Like(user.username, tweet)
-        this.likedTweets.push(likedTweet)
-        user.tweets.filter(i => i.text===tweet)[0].likes.push(this.username) //I am crying
-
-    }
 }
 
 class Tweet{
-    constructor(creator, text){
-        this.creator=creator
+    constructor(user, text){
+        this.creator =user.username
         this.text = text
-        this.likes =[]
+        this.likes = []
     }
-        
 }
 
 class Like{
-    constructor(user, text){
-        this.user = user
-        this.text = text
+    constructor(user, tweet){
+        this.user = user.username
+        this.likedTweet= tweet.text
     }
 }
 
 merve = new User('Merve', 'usagi')
 melis = new User('Melis', 'chibiusa')
 
-melis.follow(merve)
+merve.follow(melis)
+const tweet1 = merve.tweet('Hello world')
 
-merve.tweet('Hello world')
-merve.tweet('I love JS')
-melis.tweet('LOL')
-
-melis.like(merve, 'Hello world')
-melis.like(merve, 'I love JS')
+const like1 = melis.like(tweet1)
