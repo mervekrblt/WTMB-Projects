735dfc6612449e5729ab41030fd761f023ad5081
Merve Karabulut <63372370+mervekrblt@users.noreply.github.com>
2020-12-15 18:14:32 +0300

    Delete all code in index.js except database require

--- week-2/homework-2/index.js
+++ week-2/homework-2/index.js
@@ -1,29 +1,4 @@
-const User = require('./user')
-const Like = require('./like')
-const Tweet = require('./tweet')
 const Database = require('./database')
 
-
-merve = new User('Merve', 'usagi')
-melis = new User('Melis', 'chibiusa')
-
-merve.follow(melis)
-const tweet1 = merve.tweet('Hello world')
-const tweet2 = merve.tweet('I love JS')
-const tweet3 = melis.tweet('Hi')
-
-const like1 = melis.like(tweet1)
-
-console.log(melis)
-console.log(like1)
-console.log(tweet1)
-
-const users = [merve, melis]
-const tweets = [tweet1, tweet2, tweet3]
-const likes = [like1]
-
-Database.save('user.json', users)
-Database.save('tweet.json', tweets)
-Database.save('like.json', likes)
-
-console.log(Database.load('user.json'))
+console.log(Database.load('user.json')[0].tweets[0].likes)
+console.log(Database.load('tweet.json'))
