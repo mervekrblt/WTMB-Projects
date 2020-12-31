384627542072ae80ed5bd832206a11728b2ca995
Merve Karabulut <63372370+mervekrblt@users.noreply.github.com>
2020-12-31 13:17:37 +0300

    Add pug files. Render them with using render and app.set functions
    At this point, we have learned how we send data like a string with
    res.send('Hello').
    
    Secondly, how to sendFile like index.html with res.sendFile(__dirname +
    '/index.html').__dirname is for current directory.
    
    Then, if we use template like pug, I think that it is just a example for
    how exertal library works like framework(react, vue etc.), we have to
    use render. res.render('index.pug'). Whenever we asked app to render a
    view, it is gonna be a pug view like this code ---> app.set('view
    engine', 'pug').

--- week-4/lesson-4/index.js
+++ week-4/lesson-4/index.js
@@ -2,9 +2,12 @@ const express = require('express')
 
 const app = express()
 
+app.set('view engine', 'pug')
+
 app.get('/', (req,res) => {
   //res.send('/.index.html') //send --> any data like a string
-  res.sendFile('C:/Users/PC/Desktop/WTMB-Projects/week-4/lesson-4/index.html')
+  //res.sendFile(__dirname +'/index.html') --> __dirname for current directory
+  res.render('index.pug') 
 })
 
 app.listen(3000, () =>{
