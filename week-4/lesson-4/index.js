81a98d1f717b93fd2a3cea94056795f516b59d84
Merve Karabulut <63372370+mervekrblt@users.noreply.github.com>
2021-01-01 19:53:26 +0300

    Install axios library. Create post and delete functions to create/delete a person from f-end to b-end

--- week-4/lesson-4/index.js
+++ week-4/lesson-4/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const PersonService = require('./services/person-service')
+const bodyParser = require('body-parser')
 
 const app = express()
 
+app.use(bodyParser.json())
 app.set('view engine', 'pug')
 
 app.get('/', (req,res) => {
@@ -20,11 +22,25 @@ app.get('/person/all', async (req, res) => {
 app.get('/person/:id', async(req, res) => {
   const id = req.params.id
   const person = await PersonService.find(id)
-  
+
   if(person){
     res.send(person)
   }else res.send('Dont have data')
     
+})
+
+
+app.post('/person', async (req,res) =>{
+  //console.log(req.body)
+  const person = req.body
+  
+  await PersonService.add(person)
+  res.send(person) // return the person which I wrote in console
+})
+
+app.delete('/person/:id', async (req,res) =>{
+  await PersonService.del(req.params.id)
+  res.send('Data was deleted')
 })
 
 app.listen(3000, () =>{
