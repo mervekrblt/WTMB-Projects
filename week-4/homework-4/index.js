0bf28c8b3660072d50875ee7f1999527aec9969c
Merve Karabulut <63372370+mervekrblt@users.noreply.github.com>
2021-01-05 00:50:03 +0300

    Add tweet/all and like/all paths

--- week-4/homework-4/index.js
+++ week-4/homework-4/index.js
@@ -2,20 +2,47 @@ const express = require('express')
 const UserService = require('./services/user-service')
 const TweetService = require('./services/tweet-service')
 const LikeService = require('./services/like-service')
+const likeService = require('./services/like-service')
 
 const app = express()
 
 app.set('view engine', 'pug')
 
-app.get('/', (req,res)=>{
+app.get('/', (req, res) => {
     res.render('index')
 })
 
-app.get('/user/all', async (req,res)=>{
+app.get('/user/all', async (req, res) => {
     const users = await UserService.findAll()
-    res.render('user', {users:users})
+    res.render('user', {
+        users: users
+    })
 })
 
-app.listen(3000, 'localhost', ()=>{
+app.get('/user/:id', async (req, res) => {
+    const id = req.params.id
+    const user = await UserService.find(id)
+
+    if (user) {
+        res.send(user)
+    } else res.send('Dont have data')
+
+})
+
+app.get('/tweet/all', async(req,res) => {
+    const tweets = await TweetService.findAll()
+    res.render('tweet', {
+        tweets: tweets
+    })
+})
+
+app.get('/like/all', async(req,res) => {
+    const likes = await likeService.findAll()
+    res.render('like', {
+        likes: likes
+    })
+})
+
+app.listen(3000, 'localhost', () => {
     console.log('server is listening')
 })
