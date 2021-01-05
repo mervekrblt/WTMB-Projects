69fe102683924a827771d218f64db1b5f4af4ca5
Merve Karabulut <63372370+mervekrblt@users.noreply.github.com>
2021-01-05 17:17:46 +0300

    Install axios library. Create user and tweet with this library

--- week-4/homework-4/index.js
+++ week-4/homework-4/index.js
@@ -3,15 +3,22 @@ const UserService = require('./services/user-service')
 const TweetService = require('./services/tweet-service')
 const LikeService = require('./services/like-service')
 const likeService = require('./services/like-service')
-
+const bodyParser = require('body-parser')
+/*for database
+const User = require('./models/user')
+const Tweet = require('./models/tweet')
+const Like = require('./models/like')
+*/
 const app = express()
 
 app.set('view engine', 'pug')
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
     res.render('index')
 })
 
+//User
 app.get('/user/all', async (req, res) => {
     const users = await UserService.findAll()
     res.render('user', {
@@ -29,20 +36,73 @@ app.get('/user/:id', async (req, res) => {
 
 })
 
-app.get('/tweet/all', async(req,res) => {
+app.post('/user', async (req, res) => {
+    const user = req.body
+
+    await UserService.add(user)
+    res.send(user)
+})
+
+app.delete('/user/:id', async (req, res) => {
+    await UserService.del(req.params.id)
+    res.send('Data was deleted')
+})
+
+//Tweet
+app.get('/tweet/all', async (req, res) => {
     const tweets = await TweetService.findAll()
     res.render('tweet', {
         tweets: tweets
     })
 })
 
-app.get('/like/all', async(req,res) => {
-    const likes = await likeService.findAll()
-    res.render('like', {
-        likes: likes
-    })
+app.get('/tweet/:id', async (req, res) => {
+    const id = req.params.id
+    const tweet = await TweetService.find(id)
+    res.send(tweet)
 })
+
+app.post('/tweet', async (req, res) => {
+    const tweet = req.body
+
+    await TweetService.add(tweet)
+    res.send(tweet)
+})
+
+app.delete('/tweet/:id', async (req, res) => {
+    await TweetService.del(req.params.id)
+    res.send('Data was deleted')
+})
+
 
 app.listen(3000, 'localhost', () => {
     console.log('server is listening')
 })
+
+/* for database
+async function main(){
+    const merve = new User('merve', 'usagi')
+    const melis = new User('melis', 'chibiusa')
+
+     
+
+
+      const tweet1 = merve.tweet('Hello world')
+      const tweet2 = merve.tweet('I love JS')
+      const tweet3 = melis.tweet('Hello')
+      await TweetService.add(tweet1)
+      await TweetService.add(tweet2)
+      await TweetService.add(tweet3)
+      
+
+     const like1 = melis.like(tweet1)
+     const like2 = melis.like(tweet2)
+     await LikeService.add(like1)
+     await LikeService.add(like2)
+
+     await UserService.add(merve)
+     await UserService.add(melis)
+}
+
+main()
+*/
