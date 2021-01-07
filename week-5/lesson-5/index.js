50894309acde45d68dba35472453c2dbae721d07
Merve Karabulut <63372370+mervekrblt@users.noreply.github.com>
2021-01-07 10:53:48 +0300

    Add routes folder and move all person requests into this folder

--- week-5/lesson-5/index.js
+++ week-5/lesson-5/index.js
@@ -1,11 +1,14 @@
 const express = require('express')
-const PersonService = require('./services/person-service')
 const bodyParser = require('body-parser')
 
 const app = express()
 
+const personRouter = require('./routes/person')
+
 app.use(bodyParser.json())
 app.set('view engine', 'pug')
+
+app.use('/person', personRouter)
 
 app.get('/', (req,res) => {
   //res.send('/.index.html') //send --> any data like a string
@@ -13,35 +16,6 @@ app.get('/', (req,res) => {
   res.render('index.pug') 
 })
 
-app.get('/person/all', async (req, res) => {
-  const people = await PersonService.findAll()
-  //res.send(people)
-  res.render('person', {people:people})
-})
-
-app.get('/person/:id', async(req, res) => {
-  const id = req.params.id
-  const person = await PersonService.find(id)
-
-  if(person){
-    res.send(person)
-  }else res.send('Dont have data')
-    
-})
-
-
-app.post('/person', async (req,res) =>{
-  //console.log(req.body)
-  const person = req.body
-  
-  await PersonService.add(person)
-  res.send(person) // return the person which I wrote in console
-})
-
-app.delete('/person/:id', async (req,res) =>{
-  await PersonService.del(req.params.id)
-  res.send('Data was deleted')
-})
 
 app.listen(3000, () =>{
   console.log('server listening')
---
+++ week-5/lesson-5/routes/person.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const router = express.Router()
+
+const PersonService = require('../services/person-service')
+
+router.get('/all', async (req, res) => {
+    const people = await PersonService.findAll()
+    //res.send(people)
+    res.render('person', {
+        people: people
+    })
+})
+
+router.get('/:id', async (req, res) => {
+    const id = req.params.id
+    const person = await PersonService.find(id)
+
+    if (person) {
+        res.send(person)
+    } else res.send('Dont have data')
+
+})
+
+
+router.post('', async (req, res) => {
+    //console.log(req.body)
+    const person = req.body
+
+    await PersonService.add(person)
+    res.send(person) // return the person which I wrote in console
+})
+
+router.delete('/:id', async (req, res) => {
+    await PersonService.del(req.params.id)
+    res.send('Data was deleted')
+})
+
+module.exports = router
