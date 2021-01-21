ed94faf665bc9de3296fbbd70fbfd06a315aa4a6
Merve Karabulut <63372370+mervekrblt@users.noreply.github.com>
2021-01-21 20:09:33 +0300

    Add meetup test to reach %100 test-coverage result

--- week-6/lesson-6/routes/meetup.js
+++ week-6/lesson-6/routes/meetup.js
@@ -10,12 +10,26 @@ router.get('/all', async (req, res) => {
     })
 })
 
+//add for testing
+router.get('/all/json', async (req, res) => {
+    const meetup = await MeetupService.findAll()
+    res.send(meetup)
+})
+
 router.get('/:id', async (req, res) => {
     const meetup = await MeetupService.find(req.params.id)
+    if (!meetup) res.status(404)
     res.render('data', {
         data: meetup
     })
 })
+
+// add for testing
+router.get('/:id/json', async (req, res) => {
+    const meetup = await MeetupService.find(req.params.id)
+    if (!meetup) res.status(404)
+    res.send(meetup)
+  })
 
 router.post('/', async (req, res) => {
     const meetup = await MeetupService.add(req.body)
