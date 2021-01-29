d6a0a4cddb1d8dedd09db73eda1f2251e4d8c182
Merve Karabulut <63372370+mervekrblt@users.noreply.github.com>
2021-01-29 16:38:06 +0300

    Delete week-7 file

--- week-7/lesson-7/mongo-connection.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-
-async function main (){
-    await mongoose.connect("mongodb://localhost/wtm", { useUnifiedTopology: true , useNewUrlParser: true})
-    console.log("connected")
-}
-
-main()
