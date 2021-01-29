cad122820e82554d30f83b41e69cbcad522cd47d
Merve Karabulut <63372370+mervekrblt@users.noreply.github.com>
2021-01-29 18:12:05 +0300

    Install vue cli to start frontend. Install pug plain loder to use as a template

---
+++ week-7/lesson-7/frontend/src/views/Home.vue
@@ -0,0 +1,14 @@
+<script>	
+// @ is an alias to /src	
+import HelloWorld from '@/components/HelloWorld.vue'	
+export default {	
+  name: 'Home',	
+  components: {	
+    HelloWorld	
+  }	
+}	
+</script>
+
+<template lang="pug">
+  h1 Hello world
+</template>
---
+++ week-7/lesson-7/services/base-service.js
@@ -0,0 +1,25 @@
+const fs = require('fs')
+const Flatted = require('flatted');
+
+module.exports = class Service {
+
+
+  async findAll() {
+
+    return this.model.find()
+  }
+
+  async add(item) {
+
+    return this.model.create(item)
+  }
+
+  async  del(itemId) {
+
+    return this.model.remove({_id:itemId})
+  }
+
+  async find(itemId) {
+    return this.model.findById(itemId)
+  }
+}
