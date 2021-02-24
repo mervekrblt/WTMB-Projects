98b81b7b6cb1eedd72426adaee1802ee741c97e5
Merve Karabulut <63372370+mervekrblt@users.noreply.github.com>
2021-02-24 23:48:13 +0300

    Split files to backend and frontend folders

---
+++ week-8/lesson-8/frontend/src/App.vue
@@ -0,0 +1,44 @@
+<script>
+  import Footer from '@/components/footer.vue'
+
+  export default {
+    name: 'App',
+    components: {
+      Footer,
+    },
+  }
+</script>
+
+<template>
+  <div id="app">
+    <nav id="nav">
+      <router-link to="/">Meetups</router-link> |
+      <router-link to="/people">People</router-link>
+    </nav>
+    <router-view/>
+    <Footer/>
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ week-8/lesson-8/frontend/src/views/Meetup.vue
@@ -0,0 +1,38 @@
+<script>	
+// @ is an alias to /src	
+import MeetupCard from '@/components/meetup-card.vue'
+//use state properties with mapState
+import { mapState, mapActions } from 'vuex'
+
+export default {
+  name: 'Meetup',
+  components: {
+    MeetupCard
+  },
+
+  computed: {
+    ...mapState(['meetup'])
+  },
+
+  methods: {
+    ...mapActions(['fetchMeetup']),
+
+  },
+    
+  created() {
+    this.fetchMeetup(this.$route.params.id)
+  }
+}
+</script>
+
+<template lang="pug">
+ main
+    section
+        meetup-card(:meetup="meetup")
+    section Attendees:
+        p(v-for="attendees in meetup.attendees") {{ attendees.name }} 
+</template>
+
+<style  scoped>
+
+</style>
