34495a405ca1ba028b837aca0e465aaa3f96f09b
Merve Karabulut <63372370+mervekrblt@users.noreply.github.com>
2021-02-24 21:02:07 +0300

    Delete week-8

--- week-8/lesson-8/backend/index.js
+++
@@ -1,7 +0,0 @@
-
-const app = require('./app')
-
-
-app.listen(process.env.PORT || 3000, () => {
-  console.log("Server listening");
-});
--- week-8/lesson-8/backend/services/meetup-service.js
+++
@@ -1,8 +0,0 @@
-const BaseService = require('./base-service')
-const MeetupModel = require('../models/meetup')
-
-class MeetupService extends BaseService {
-    model= MeetupModel
-}
-
-module.exports = new MeetupService()
